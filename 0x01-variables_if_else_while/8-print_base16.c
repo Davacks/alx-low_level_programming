94a5130391db9ebaea1df7e05b3bdeefbcefd367
David-Den-Dinho <egbodavid123@gmail.com>
2022-04-08 05:05:44 -0700

    Mandatory task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print the numbers of base 16 in lowercase
+ * main - Prints the numbers of base 16 in lowercase
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 		char digit;
 
-		for (digit = '0'; digit <= '9' digit++)
+		for (digit = '0'; digit <= '9'; digit++)
 		putchar(digit);
 
 		for (digit = 'a'; digit <= 'f'; digit++)
