e2f755ccffb1532d46fcf5824d76aaeb42079baf
David-Den-Dinho <egbodavid123@gmail.com>
2022-04-08 08:05:45 -0700

    Advance Task fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - main block 
+ * main - main block
  * Description: Write a program that prints all
  * different possible combinationa of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int tens;
-int ones;
+	int c;
+	int d;
+	int e = 0;
 
-for (tens = 0; tens <= 9; tens++)
-{
-for (ones = tens + 1; ones <= 9; ones++)
-{
-putchar(tens + '0');
-putchar(ones + '0');
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
 
-if (tens < 0)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-
-return (0);
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
