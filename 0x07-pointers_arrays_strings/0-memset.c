5436ba3d038c542c6bbed15252af52cab03b3886
David-Den-Dinho <egbodavid123@gmail.com>
2022-04-26 12:41:00 -0700

    Mandatory Task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset - fills a block of memory with a specific value
+ * @s: the starting address of the memory to be filled
+ * @b: pointer variable
+ * @n: pointer variable
+ *
+ * Return: changed array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int i)
+{
+	int i = 0;
+
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
