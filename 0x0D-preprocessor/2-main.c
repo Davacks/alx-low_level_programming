2a419fccaf6cd23f63532d8ebd1cde67b0563cd8
David-Den-Dinho <egbodavid123@gmail.com>
2022-05-11 16:25:25 -0700

    Mandatory Task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, to check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
